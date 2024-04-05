a921b9b462d12f4cd97fc2afd06821261438be68
용택 권 <jelly@retgames.co.kr>
2024-04-05 18:40:47 +0900

    ..

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="m-4">
+    <h1 class="text-green-500 text-4xl font-bold px-20">
+        Hello World
+    </h1>
+    <string>Tailwind Css tutorial</string>
+    <p>
+        You can use Tailwind CSS as a replacement of CSS,
+        this is a framework that increase your pace to
+        design any website.
+    </p>
+</div>
+
