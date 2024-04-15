cfdd440abde25f27d1ddb6291998e5de0e136bd9
yongteak <skillove@gmail.com>
2024-04-15 23:12:14 +0900

    ..

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,156 @@
-<script>
+<script lang="ts">
   import "../app.pcss";
+  import Triangle from "lucide-svelte/icons/triangle";
+  import Bot from "lucide-svelte/icons/bot";
+  import SquareTerminal from "lucide-svelte/icons/square-terminal";
+  import CodeXML from "lucide-svelte/icons/code-xml";
+  import Settings2 from "lucide-svelte/icons/settings-2";
+  import LifeBuoy from "lucide-svelte/icons/life-buoy";
+  import Book from "lucide-svelte/icons/book";
+  import SquareUser from "lucide-svelte/icons/square-user";
+  import Settings from "lucide-svelte/icons/settings";
+  import Rabbit from "lucide-svelte/icons/rabbit";
+  import Bird from "lucide-svelte/icons/bird";
+  import Turtle from "lucide-svelte/icons/turtle";
+  import Share from "lucide-svelte/icons/share";
+  import Paperclip from "lucide-svelte/icons/paperclip";
+  import Mic from "lucide-svelte/icons/mic";
+  import CornerDownLeft from "lucide-svelte/icons/corner-down-left";
+
+  import { Badge } from "$lib/components/ui/badge/index.js";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import * as Tooltip from "$lib/components/ui/tooltip/index.js";
+  import * as Drawer from "$lib/components/ui/drawer/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Textarea } from "$lib/components/ui/textarea/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+  import * as Select from "$lib/components/ui/select/index.js";
 </script>
 
-<header class="fixed top-0 left-0 w-full flex justify-center items-center bg-gray-800 text-white h-12">
-  <nav>
-    <ul class="flex space-x-4">
-      <li><a href="/">Home</a></li>
-      <li><a href="/main">Main</a></li>
-      <li><a href="/new">New</a></li>
-      <li><a href="/comment">Comment</a></li>
-    </ul>
-  </nav>
-</header>
+<div class="grid h-screen w-full pl-[53px]">
+  <aside class="inset-y fixed left-0 z-20 flex h-full flex-col border-r">
+    <div class="border-b p-2">
+      <Button variant="outline" size="icon" aria-label="Home">
+        <Triangle class="size-5 fill-foreground" />
+      </Button>
+    </div>
+    <nav class="grid gap-1 p-2">
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <Button
+            variant="ghost"
+            size="icon"
+            class="rounded-lg bg-muted"
+            aria-label="Playground"
+            builders={[builder]}
+          >
+            <SquareTerminal class="size-5" />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right" sideOffset={5}>Playground</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <Button
+            variant="ghost"
+            size="icon"
+            class="rounded-lg"
+            aria-label="Models"
+            builders={[builder]}
+          >
+            <Bot class="size-5" />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right" sideOffset={5}>Models</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <Button
+            variant="ghost"
+            size="icon"
+            class="rounded-lg"
+            aria-label="API"
+            builders={[builder]}
+          >
+            <CodeXML class="size-5" />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right" sideOffset={5}>API</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <Button
+            variant="ghost"
+            size="icon"
+            class="rounded-lg"
+            aria-label="Documentation"
+            builders={[builder]}
+          >
+            <Book class="size-5" />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right" sideOffset={5}>Documentation</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <Button
+            variant="ghost"
+            size="icon"
+            class="rounded-lg"
+            aria-label="Settings"
+            builders={[builder]}
+          >
+            <Settings2 class="size-5" />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right" sideOffset={5}>Settings@</Tooltip.Content>
+      </Tooltip.Root>
+    </nav>
+    <nav class="mt-auto grid gap-1 p-2">
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <Button
+            variant="ghost"
+            size="icon"
+            class="mt-auto rounded-lg"
+            aria-label="Help"
+            builders={[builder]}
+          >
+            <LifeBuoy class="size-5" />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right" sideOffset={5}>Help</Tooltip.Content>
+      </Tooltip.Root>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <Button
+            variant="ghost"
+            size="icon"
+            class="mt-auto rounded-lg"
+            aria-label="Account"
+            builders={[builder]}
+          >
+            <SquareUser class="size-5" />
+          </Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right" sideOffset={5}>Account</Tooltip.Content>
+      </Tooltip.Root>
+    </nav>
+  </aside>
+  <div class="flex flex-col">
+    <header
+      class="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4"
+    >
+      <h1 class="text-xl font-semibold">Playground</h1>
+    </header>
+    <!-- <main class="flex-1 overflow-y-auto"> -->
+      <slot />
+    <!-- </main> -->
+  </div>
+</div>
 
-<main class="mt-12 p-4">
-  <slot />
-</main>
+<!-- </main>
 
 <footer class="fixed bottom-0 left-0 w-full bg-gray-800 text-white text-center p-4">
   © 2023 Your Company
-</footer>
+</footer> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,136 @@
-<center> 
-    <h1 class="text-green-600 text-5xl font-bold"> 
-        GeeksforGeeks 
-    </h1>  
-    <b>Tailwind CSS object Class</b>  
-    <div class="bg-green-300 w-full h-full"> 
-    <img class="object-contain h-48 w-full" 
-         src= 
-"https://media.geeksforgeeks.org/wp-content/uploads/20190807114330/GFG115.png">     
-  
-</center> 
+<script lang="ts">
+    import "../app.pcss";
+    import Settings from "lucide-svelte/icons/settings";
+    import Rabbit from "lucide-svelte/icons/rabbit";
+    import Bird from "lucide-svelte/icons/bird";
+    import Turtle from "lucide-svelte/icons/turtle";
+      
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Drawer from "$lib/components/ui/drawer/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Textarea } from "$lib/components/ui/textarea/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+    import * as Select from "$lib/components/ui/select/index.js";
+  </script>
+  <h1>wow</h1>
+<Drawer.Root>
+    <Drawer.Trigger asChild let:builder>
+      <Button builders={[builder]} variant="ghost" size="icon" class="md:hidden">
+        <Settings class="size-4" />
+        <span class="sr-only">Settings</span>
+      </Button>
+    </Drawer.Trigger>
+    <Drawer.Content class="max-h-[80vh]">
+      <Drawer.Header>
+        <Drawer.Title>Configuration</Drawer.Title>
+        <Drawer.Description>
+          Configure the settings for the model and messages.
+        </Drawer.Description>
+      </Drawer.Header>
+      <form class="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
+        <fieldset class="grid gap-6 rounded-lg border p-4">
+          <legend class="-ml-1 px-1 text-sm font-medium"> Settings </legend>
+          <div class="grid gap-3">
+            <Label for="model">Model</Label>
+            <Select.Root>
+              <Select.Trigger
+                id="model"
+                class="items-start [&_[data-description]]:hidden"
+              >
+                <Select.Value placeholder="Select a model" />
+              </Select.Trigger>
+              <Select.Content>
+                <Select.Item value="genesis" label="Neural Genesis">
+                  <div
+                    class="flex items-start gap-3 text-muted-foreground"
+                  >
+                    <Rabbit class="size-5" />
+                    <div class="grid gap-0.5">
+                      <p>
+                        Neural
+                        <span class="font-medium text-foreground">
+                          Genesis
+                        </span>
+                      </p>
+                      <p class="text-xs" data-description>
+                        Our fastest model for general use cases.
+                      </p>
+                    </div>
+                  </div>
+                </Select.Item>
+                <Select.Item value="explorer" label="Neural Explorer">
+                  <div
+                    class="flex items-start gap-3 text-muted-foreground"
+                  >
+                    <Bird class="size-5" />
+                    <div class="grid gap-0.5">
+                      <p>
+                        Neural
+                        <span class="font-medium text-foreground">
+                          Explorer
+                        </span>
+                      </p>
+                      <p class="text-xs" data-description>
+                        Performance and speed for efficiency.
+                      </p>
+                    </div>
+                  </div>
+                </Select.Item>
+                <Select.Item value="quantum" label="Neural Quantum">
+                  <div
+                    class="flex items-start gap-3 text-muted-foreground"
+                  >
+                    <Turtle class="size-5" />
+                    <div class="grid gap-0.5">
+                      <p>
+                        Neural
+                        <span class="font-medium text-foreground">
+                          Quantum
+                        </span>
+                      </p>
+                      <p class="text-xs" data-description>
+                        The most powerful model for complex
+                        computations.
+                      </p>
+                    </div>
+                  </div>
+                </Select.Item>
+              </Select.Content>
+            </Select.Root>
+          </div>
+          <div class="grid gap-3">
+            <Label for="temperature">Temperature</Label>
+            <Input id="temperature" type="number" placeholder="0.4" />
+          </div>
+          <div class="grid gap-3">
+            <Label for="top-p">Top P</Label>
+            <Input id="top-p" type="number" placeholder="0.7" />
+          </div>
+          <div class="grid gap-3">
+            <Label for="top-k">Top K</Label>
+            <Input id="top-k" type="number" placeholder="0.0" />
+          </div>
+        </fieldset>
+        <fieldset class="grid gap-6 rounded-lg border p-4">
+          <legend class="-ml-1 px-1 text-sm font-medium"> Messages </legend>
+          <div class="grid gap-3">
+            <Label for="role">Role</Label>
+            <Select.Root selected={{ value: "system", label: "System" }}>
+              <Select.Trigger>
+                <Select.Value placeholder="Select a role" />
+              </Select.Trigger>
+              <Select.Content>
+                <Select.Item value="system">System</Select.Item>
+                <Select.Item value="user">User</Select.Item>
+                <Select.Item value="assistant">Assistant</Select.Item>
+              </Select.Content>
+            </Select.Root>
+          </div>
+          <div class="grid gap-3">
+            <Label for="content">Content</Label>
+            <Textarea id="content" placeholder="You are a..." />
+          </div>
+        </fieldset>
+      </form>
+    </Drawer.Content>
+  </Drawer.Root>
